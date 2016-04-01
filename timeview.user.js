2eafe963bbe6b1a8cd70324b2a14ed00ca1e9655
Sam Persson <samiljan@gmail.com>
2016-04-01 13:17:30 +0200

    Create timeview.user.js

---
+++ timeview.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        ProjectCompanion tidsvy
+// @namespace   projectcompanion.keyboardextensions
+// @include     https://host*.projectcompanion.com/ProjectCompanion/application/time/timeview.asp*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+window.addEventListener("keydown", function(event) {
+  
+  if (event.defaultPrevented) {
+    return; // Should do nothing if the key event was already consumed.
+  }
+  
+  console.log("keydown", event);
+  
+  switch (event.code) {
+    case "KeyW":
+      if(!event.altKey && !event.ctrlKey && !event.shiftKey) {
+        console.log("KeyW", OnBnNew);
+        OnBnNew(); 
+        event.preventDefault();
+      }
+      break;
+    default:
+      return; // Quit when this doesn't handle the key event.
+  }
+}, true);
